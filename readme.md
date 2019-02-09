a0694f7532fdff57cbf4526790872e857a48e4fe
adamkulawik <adamkulawik@wp.pl>
2019-02-09 19:05:30 +0100

    String isblank() elaborated

--- readme.md
+++ readme.md
@@ -1,17 +1,27 @@
 
+###
+
+#### String
+
+##### isBlank()
+
+JDK Bug System: https://bugs.openjdk.java.net/browse/JDK-8200437
+
+
+
 ### Files
 
 #### readString()
 #### writeString()
 #### isSameContent()
 
-Specification: https://bugs.openjdk.java.net/browse/JDK-8201276
+JDK Bug System: https://bugs.openjdk.java.net/browse/JDK-8201276
 
 Read more: https://dzone.com/articles/new-jdk-11-files-methods-for-readingwriting-string
 
 ### Predicate
 #### static not(Predicate)
 
-Specification: https://bugs.openjdk.java.net/browse/JDK-8203428
+JDK Bug System: https://bugs.openjdk.java.net/browse/JDK-8203428
 
 Read more: https://dzone.com/articles/predicatenot-coming-to-java
--- src/test/java/pl/sjug/java11/string/IsBlankTest.java
+++ src/test/java/pl/sjug/java11/string/IsBlankTest.java
@@ -7,23 +7,25 @@ import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 public class IsBlankTest {
 
     @Test
-    public void shouldBeBlankButNotEmpty() {
+    public void shouldBeBlankButIsNotEmpty() {
         var blankNonEmpty = "\u2005    \n\t";
-        assertThat(blankNonEmpty.isBlank()).isTrue();
-        assertThat(blankNonEmpty.isEmpty()).isFalse();
+        assertThat(blankNonEmpty.isBlank()).isTrue();           // Java 11
+        assertThat(blankNonEmpty.trim().isEmpty()).isFalse();   // Pre - Java 8
+        assertThat(blankNonEmpty.codePoints().allMatch(Character::isWhitespace)).isTrue();   // Java 8
+
     }
 
    @Test
     public void shouldBeBlankAndEmpty() {
         var blankNonEmpty = "";
-        assertThat(blankNonEmpty.isBlank()).isTrue();
-        assertThat(blankNonEmpty.isEmpty()).isTrue();
+        assertThat(blankNonEmpty.isBlank()).isTrue();           // Java 11
+        assertThat(blankNonEmpty.isEmpty()).isTrue();           // Pre - Java 11
     }
 
     @Test
     public void shouldBeNeitherBlankNotEmpty() {
-        var notBlanNorEmpty = "aaa";
-        assertThat(notBlanNorEmpty.isBlank()).isFalse();
-        assertThat(notBlanNorEmpty.isEmpty()).isFalse();
+        var notBlankNorEmpty = "aaa";
+        assertThat(notBlankNorEmpty.isBlank()).isFalse();       // Java 11
+        assertThat(notBlankNorEmpty.isEmpty()).isFalse();       // Pre - Java 11
     }
 }
