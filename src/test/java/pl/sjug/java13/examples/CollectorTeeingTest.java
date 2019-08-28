90787879b6094e544fb2d794777d000f62fb2d8a
Adam Kulawik <adamkulawik@wp.pl>
2019-08-29 00:03:46 +0200

    collector teeing done as test

--- src/test/java/pl/sjug/java13/examples/CollectorTeeingTest.java
+++ src/test/java/pl/sjug/java13/examples/CollectorTeeingTest.java
@@ -1,20 +1,30 @@
 package pl.sjug.java13.examples;
 
+import org.junit.Test;
+
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.counting;
 import static java.util.stream.Collectors.summingDouble;
 import static java.util.stream.Collectors.teeing;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class CollectorTeeingTest {
 
-    public static void main(String[] args) {
-        double average = Stream.of(1, 4, 2, 7, 4, 6, 5)
-                               .collect(teeing(
-                                       summingDouble(i -> i),
-                                       counting(),
-                                       (sum, n) -> sum / n));
 
-        System.out.println(average);
+    @Test
+    public void collectorTeeing() {
+        // given
+        Stream<Integer> inputStream = Stream.of(1, 2, 3, 4, 5, 6);
+
+        // when
+        double result = inputStream
+                .collect(teeing(
+                        summingDouble(i -> i),
+                        counting(),
+                        (sum, n) -> sum / n));
+
+        // then
+        assertThat(result).isEqualTo(3.5);
     }
 }
